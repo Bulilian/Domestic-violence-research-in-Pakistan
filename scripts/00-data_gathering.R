3a6523026e8758fa87a2a494add349f56200d227
Bulilian <buxulilian@gmail.com>
2022-04-27 21:49:16 -0400

    Update discussion and reference

--- scripts/00-data_gathering.R
+++ scripts/00-data_gathering.R
@@ -1,14 +1,9 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
+# Purpose: https://dhsprogram.com/pubs/pdf/FR290/FR290.pdf
 # Author: Bu Xu
 # Data: Apr 18 2022
 # Contact: bu.xu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
 
 
 #### Workspace setup ####
--- scripts/02-simulation.R
+++ scripts/02-simulation.R
@@ -1,14 +1,10 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
+# Purpose: Simulation
 # Author: Bu Xu
 # Data: Apr 18 2022
 # Contact: bu.xu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
+
 
 
 #### Workspace setup ####
@@ -17,3 +13,17 @@ library(tidyverse)
 library(rvest)
 library(pdftools)
 library(tibble)
+
+#### Simulate ####
+set.seed(499)
+
+simulated_data <- 
+  tibble('living chilren' = sample( x = c(
+    '0', '1-2', '3-4', '5+'),
+    size = 3687, 
+    replace = T
+  ))
+simulated_data
+
+table(simulated_data)
+write_csv(simulated_data, file = "outputs/simul.csv")
